e105dfa723754353f74686355695b685eba81d5a
shayan <syedshayan1324@gmail.com>
2024-08-10 16:28:47 +0500

    fixed for mobile devices

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-if (window.innerWidth > 500) {
-    window.addEventListener("resize", () => window.location.reload());
-}
-
 (function () {
     const locomotiveScroll = new LocomotiveScroll();
 })();
 
+function getDeviceType() {
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        return
+    } else {
+        window.addEventListener("resize", () => window.location.reload());
+    }
+}
+getDeviceType()
+
 let vdoSrc = [
-    'https://videos.pexels.com/video-files/8466294/8466294-uhd_2560_1440_25fps.mp4',
-    'https://videos.pexels.com/video-files/15562120/15562120-hd_1920_1080_24fps.mp4',
-    'https://videos.pexels.com/video-files/7710243/7710243-uhd_2560_1440_30fps.mp4',
-    'https://videos.pexels.com/video-files/9669050/9669050-hd_1920_1080_25fps.mp4',
-    'https://videos.pexels.com/video-files/3129957/3129957-uhd_2560_1440_25fps.mp4',
+    'https://videos.pexels.com/video-files/8466294/8466294-hd_1280_720_25fps.mp4',
+    'https://videos.pexels.com/video-files/15562120/15562120-hd_1280_720_24fps.mp4',
+    'https://videos.pexels.com/video-files/7710243/7710243-hd_1280_720_30fps.mp4',
+    'https://videos.pexels.com/video-files/9669050/9669050-hd_1280_720_25fps.mp4',
 ]
 function randVdo() {
-    let rnd = Math.floor(Math.random() * 5);
+    let rnd = Math.floor(Math.random() * 4);
     select(".vdo").setAttribute("src", vdoSrc[rnd])
 }
 function select(x) {
@@ -47,45 +51,89 @@ function pageOneAnim() {
             trigger: '.home',
             start: 'top top',
             end: 'bottom bottom',
-            scrub: 2,
+            scrub: 1,
         }
     });
 
     tl.to(".layer1", {
         "--circ": "0%",
-        ease: Power2,
-        duration: 3
+        duration: 4
     }, 'a')
         .to(".scaler", {
             scale: 1,
             ease: Power1,
-            duration: 3
+            duration: 4
         }, 'a')
         .to(".rowlft", {
-            xPercent: 5,
+            xPercent: 6,
             stagger: 0.1,
-            duration: 8
+            duration: 5
         }, 'b')
         .to(".rowrgt", {
             xPercent: -8,
             stagger: 0.1,
-            duration: 8
+            duration: 5,
         }, 'b')
         .to(".headText span", {
             opacity: 0.8,
             stagger: 0.1,
         }, 'b')
 }
+function cyanCardsAnim(){
+    if (window.innerWidth <= 1024) {
+        document.querySelectorAll(".card").forEach(e => {
+            gsap.to(e, {
+                scrollTrigger: {
+                    trigger: e,
+                    start: 'top 60%',
+                    end: 'top 60%',
+                    scrub: 1,
+                },
+                'background': '#000000',
+                'color': '#AEDEE0',
+            })
+        })
+    
+    } else {
+        gsap.set(".card", { scaleX: 1 })
+        document.querySelectorAll(".card").forEach(e => {
+            gsap.to(e, {
+                scrollTrigger: {
+                    trigger: e,
+                    scrub: true,
+                    start: 'top 50%',
+                    end: 'bottom 50%',
+                },
+                scaleX: 1.08,
+                'background': '#000000',
+                'color': '#AEDEE0',
+                duration: 1,
+                ease: Power4
+            })
+            gsap.to(".capsule", {
+                scrollTrigger: {
+                    trigger: ".capsule",
+                    start: "top 50%",
+                    end: 'bottom 50%',
+                    scrub: true
+                },
+                'transform': 'translateY(-3.5rem)',
+                stagger: 0.2,
+                ease: Power2,
+                duration: 3
+            })
+        })
+    }
+}
 function xScroller() {
     gsap.to(".slide", {
         scrollTrigger: {
             trigger: ".trigger",
-            start: `18% top`,
-            end: `bottom bottom`,
+            start: '35% top',
+            end: 'bottom bottom',
             scrub: 2,
         },
         xPercent: -100,
-        // ease: Power1
     })
 }
 function themeChanger() {
@@ -111,45 +159,55 @@ function verticalPage3Anim(dir) {
         scrollTrigger: {
             trigger: ".trigger",
             start: `top 10%`,
-            end: `50% bottom`,
+            end: `top bottom`,
+            // markers:true,
             scrub: 1,
         }
     })
     tl2
         .to(".capsSkillsRgt", { [dir]: 160 }, "skl")
         .to(".capsSkill", { [dir]: 50 }, "skl")
-        .to(".capsSkillsLft", { [dir]: amount }, "skl")
-        .to(".capsSkillsText", {
-            scrollTrigger: {
-                trigger: ".trigger",
-                start: `top 10%`,
-                end: `50% bottom`,
-                scrub: 0.5,
-            },
-            opacity: 0,
-            scale: 0,
-            stagger: 0.1
-        }).from(".skillIcon", {
-            scrollTrigger: {
-                trigger: ".trigger",
-                start: `top 10%`,
-                end: `50% bottom`,
-                scrub: 1,
-            },
-            opacity: 0,
-            scale: 0,
-        })
+        .to(".capsSkillsLft", { [dir]: amount }, "skl");
+
+    let tl3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".trigger",
+            start: `top 10%`,
+            end: `50% bottom`,
+            scrub: 0.5,
+        }
+    })
+    tl3.to(".capsSkillsText", {
+        opacity: 0,
+        scale: 0,
+        stagger: 0.1
+    }).from(".skillIcon", {
+        opacity: 0,
+        scale: 0,
+    })
     gsap.from(".projectCard", {
         scrollTrigger: {
             trigger: ".trigger",
-            start: 'bottom-=50 65%',
-            end: 'bottom 65%',
-            scrub: true,
+            start: 'bottom 70%',
+            end: 'bottom 70%',
+            scrub: 1,
         },
         x: 100,
         opacity: 0,
-        stagger: 0.05
-    })
+        stagger: 0.05,
+        ease: Power4
+    }, "bgb")
+    gsap.from(".prjCardsBg", {
+        scrollTrigger: {
+            trigger: ".trigger",
+            start: 'bottom 70%',
+            end: 'bottom 70%',
+            scrub: 1,
+            // markers:true
+        },
+        opacity: 0,
+        duration: 3
+    }, "bgb")
 }
 function textAnim(x, classes = "") {
     if (window.innerWidth < 670) {
@@ -165,21 +223,21 @@ function textAnim(x, classes = "") {
 textAnim(".headText");
 textAnim(".nixipixi", "relative");
 
-document.addEventListener("DOMContentLoaded", function () {
-    randVdo();
-    navButtonToggler();
-    pageOneAnim()
-    xScroller();
-    themeChanger();
-    if (window.innerWidth <= 768) {
-        verticalPage3Anim('y');
-    } else {
-        verticalPage3Anim('x');
-    }
-});
+
+randVdo();
+navButtonToggler();
+pageOneAnim()
+cyanCardsAnim();
+xScroller();
+themeChanger();
+
+if (window.innerWidth <= 768) {
+    verticalPage3Anim('y');
+} else {
+    verticalPage3Anim('x');
+}
 
 gsap.set(".nixipixi span", { 'top': '50' })
-
 gsap.to(".nixipixi span", {
     scrollTrigger: {
         trigger: ".page4",
@@ -192,46 +250,3 @@ gsap.to(".nixipixi span", {
     stagger: 0.04,
     ease: 'circ'
 })
-
-
-if (window.innerWidth <= 1024) {
-    gsap.set(".card", { scale: 1 })
-    gsap.to(".cardCont", {
-        scrollTrigger: {
-            trigger: ".pg2Sec",
-            start: 'top -10%',
-            scrub: true,
-            pin: true,
-        },
-        x: "-75%"
-    }, 'c')
-
-} else {
-    document.querySelectorAll(".card").forEach(e => {
-        gsap.to(e, {
-            scrollTrigger: {
-                trigger: e,
-                scrub: true,
-                start: 'top 50%',
-                end: 'bottom 50%',
-            },
-            scaleX: 1.08,
-            'background': '#000000',
-            'color': '#AEDEE0',
-            duration: 1,
-            ease: Power4
-        })
-        gsap.to(".capsule", {
-            scrollTrigger: {
-                trigger: ".capsule",
-                start: "top 50%",
-                end: 'bottom 50%',
-                scrub: true
-            },
-            'transform': 'translateY(-3.5rem)',
-            stagger: 0.2,
-            ease: Power2,
-            duration: 3
-        })
-    })
-}
